fc704039fa4477ff1c47e8e0dcefe96111c3bdd7
Ravinder Singh <ravindermaan0123@gmail.com>
2021-06-27 14:40:02 +0530

    formatted all files

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,24 +1,25 @@
 class UsersController < ApplicationController
-	skip_before_action :verify_authenticity_token
-	def index
-		# render plain: "Hurrey i am working"
-		render plain: User.all.map { |x| x.to_pleasant_string}.join("\n")
-	end
+  skip_before_action :verify_authenticity_token
 
-	def create
-		names = params[:name]
-		email = params[:email]
-		password = params[:password]
-		new_user = User.create!(name: names,email:email,password:password)
-		new_user.save!
-		render plain: "new user created with id: #{new_user.id}"
-	end
+  def index
+    # render plain: "Hurrey i am working"
+    render plain: User.all.map { |x| x.to_pleasant_string }.join("\n")
+  end
 
-	def login
-		email = params[:email]
-		password = params[:password]
-		found_user = User.where(email: email,password:password)
-		response_text = (found_user.count == 0) ? false : true
-		render plain: response_text
-	end
+  def create
+    names = params[:name]
+    email = params[:email]
+    password = params[:password]
+    new_user = User.create!(name: names, email: email, password: password)
+    new_user.save!
+    render plain: "new user created with id: #{new_user.id}"
+  end
+
+  def login
+    email = params[:email]
+    password = params[:password]
+    found_user = User.where(email: email, password: password)
+    response_text = (found_user.count == 0) ? false : true
+    render plain: response_text
+  end
 end
