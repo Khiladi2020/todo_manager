8e230a0b41192a4a1905c7efd17805c9ebd67a3e
Ravinder Singh <ravindermaan0123@gmail.com>
2021-06-27 14:17:39 +0530

    add new user functionality completed

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,4 +4,14 @@ class UsersController < ApplicationController
 		# render plain: "Hurrey i am working"
 		render plain: User.all.map { |x| x.to_pleasant_string}.join("\n")
 	end
+
+	def create
+		names = params[:name]
+		email = params[:email]
+		password = params[:password]
+		new_user = User.create!(name: names,email:email,password:password)
+		new_user.save!
+		render plain: "new user created with id: #{new_user.id}"
+	end
+
 end
