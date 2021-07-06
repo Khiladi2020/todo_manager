d0017850d9fac172e6cd87b8e012b36e56e5670d
Ravinder Singh <ravindermaan0123@gmail.com>
2021-07-06 23:39:53 +0530

    integrated todos erb template to rails backend

--- app/models/todo.rb
+++ app/models/todo.rb
@@ -1,4 +1,20 @@
 class Todo < ActiveRecord::Base
+  def self.overdue
+    all.where("due_date < ? AND completed = ?",Date.today,false)
+  end
+
+  def self.due_today
+    all.where("due_date = ?",Date.today)
+  end
+  
+  def self.due_later
+    all.where("due_date > ?",Date.today)
+  end
+
+  def self.completed
+    all.where(completed:true)
+  end
+
   def to_pleasant_string
     is_completed = completed ? "[X]" : "[ ]"
     "#{id}) #{due_date.to_s(:long)} #{todo_text} #{is_completed}"
