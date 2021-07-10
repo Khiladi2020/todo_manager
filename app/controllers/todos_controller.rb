0335546887946df12ac6b4355f8912ae0318a062
Ravinder Singh <ravindermaan0123@gmail.com>
2021-07-10 22:50:58 +0530

    added delete todo functionionalty using trash icon

--- app/controllers/todos_controller.rb
+++ app/controllers/todos_controller.rb
@@ -30,4 +30,11 @@ class TodosController < ApplicationController
     # render plain: "Completion status updated for todo #{id} to #{completed}"
     redirect_to todos_path
   end
+
+  def destroy
+    id = params[:id]
+    todo = Todo.find(id)
+    todo.destroy
+    redirect_to todos_path
+  end
 end
